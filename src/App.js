4dc0e61cc2f5d51e9d27870296487e3def223b54
kravchenkoo <kravchenkoo14@gmail.com>
2024-09-04 23:17:03 +0300

    lecture 71

--- src/App.js
+++ src/App.js
@@ -1,3 +1,10 @@
+import React, { useState } from 'react';
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: true },
+  { id: 3, description: "Charger", quantity: 1, packed: false },
+]
+
 export default function App() {
   return (
     <div className="app">
@@ -13,14 +20,42 @@ function Logo() {
   return <h1>🌴 Far Away 💼</h1>;
 }
 function Form() {
+  const [formData, setFormData] = useState(''); 
+
+  function handleSubmit(e) {
+    e.preventDefault()
+  }
+
   return (
-    <div className="add-form">
+    <form className="add-form" onSubmit={handleSubmit}>
       <h3>What do you need for your 😍 treep</h3>
-    </div>
+      <select>
+        {Array.from({length: 20}, (_, i) => i +1 ).map((num) => <option value={num} key={num}>{num}</option>)}
+      </select>
+      <input type="text" placeholder="Item..." />
+      <button>Add</button>
+    </form>
   );
 }
 function PackingList() {
-  return <div className="list">List</div>;
+  return (
+    <div className="list">
+      <ul>
+        {initialItems.map((item) => <Item item={item} key={item.id} />)}
+      </ul>
+    </div>
+  );
+}
+function Item({item}) {
+  return (
+    <li>
+      <span 
+        style={item.packed ? {textDecoration: "line-through"}: {}}
+      >
+        {item.description} {item.quantity}</span>
+      <button>❌</button>
+    </li>
+  )
 }
 function Stats() {
   return (
@@ -29,3 +64,4 @@ function Stats() {
     </footer>
   );
 }
+
